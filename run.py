1517a45c2be4a19dab35da8e0155d19dfd705262
Yossi <78090344+Yossi345@users.noreply.github.com>
2023-04-21 10:18:13 +0300

    Update run.py

--- run.py
+++ run.py
@@ -1,24 +1,24 @@
 import boto3
+
+# Create a connection to the EC2 service
 ec2 = boto3.client('ec2')
-response = ec2.describe_instances(
-    Filters=[
-        {
-            'Name': 'instance-state-code',
-            'Values': ['80']
-        },
-#        {
-#            'Name': 'tag:k8s.io/role/master',
-#            'Values': ['1']
-#        }
-    ]
-)
 
-for each_reservation in response["Reservations"]: 
-    for each_instance in each_reservation["Instances"]:
-        print("Reserved_id:{}\tinstance_id:{}".format(
-            each_instance["Value"],
-            each_instance["InstanceId"])) 
+# Define the filters for the query
+filters = [
+    {'Name': 'tag:k8s.io/role/master', 'Values': ['1']},
+    {'Name': 'instance-state-code', 'Values': ['80']}
+]
 
+# Use the filters to get a list of instances
+response = ec2.describe_instances(Filters=filters)
 
-if __name__ == '__main__':
-    list_running_instances()
+# Loop through the instances and print the instance ID and name
+for reservation in response['Reservations']:
+    for instance in reservation['Instances']:
+        instance_id = instance['InstanceId']
+        instance_name = ''
+        for tag in instance['Tags']:
+            if tag['Key'] == 'Name':
+                instance_name = tag['Value']
+                break
+        print(f"Instance ID: {instance_id}\tInstance Name: {instance_name}")
